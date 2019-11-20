426eb709edf19a729623ea6f2f7c101a46a6cc87
Aleksander Henriksen <alhen18@student.sdu.dk>
2019-11-20 20:23:38 +0100

    database and start of backend

---
+++ Backend/backend.py
@@ -0,0 +1,9 @@
+FROM flask import Flask
+import urllib.request, json
+
+@app.route('/select')
+def get
+
+
+@app.route('/insert')
+def post
---
+++ Database/init.sql
@@ -0,0 +1,8 @@
+create database person;
+use person;
+
+CREATE TABLE persons (
+PersonID int AUTO_INCREMENT PRIMARY KEY,
+FirstName VARCHAR(255) NOT NULL,
+LastName VARCHAR(255) NOT NULL
+);
