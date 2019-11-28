95682b1e6b4c6707a25216c35233817ab60fb7bb
Aleksander Henriksen <alhen18@student.sdu.dk>
2019-11-28 13:29:29 +0100

    SSL

--- Database/init.sql
+++ Database/init.sql
@@ -2,7 +2,7 @@ create database person;
 use person;
 
 CREATE TABLE persons (
-PersonID int AUTO_INCREMENT PRIMARY KEY,
-FirstName VARCHAR(255) NOT NULL,
-LastName VARCHAR(255) NOT NULL
+  PersonID int AUTO_INCREMENT PRIMARY KEY,
+  FirstName VARCHAR(255) NOT NULL,
+  LastName VARCHAR(255) NOT NULL
 );
