de49cad8163f72c1519728ee944fc6ff17f95fa2
Aleksander Henriksen <alhen18@student.sdu.dk>
2019-12-07 13:13:49 +0100

    hey

--- Backend/backend.py
+++ Backend/backend.py
@@ -2,6 +2,8 @@ from flask import Flask, request, json
 
 import mysql.connector
 
+app = Flask(__name__)
+
 def connection():
     cnx = mysql.connector.connect(
         hostname="database",
@@ -12,32 +14,33 @@ def connection():
     )
     return cnx
 
-def setCursor():
-    return connection().cursor()
-
-app = Flask(__name__)
-
 @app.route('/persons', method=["GET"])
 def get():
-    setCursor().exectue("SELECT * FROM persons")
-    myResault = setCursor.fetchall()
+    db = connection()
+    cursor = db.cursor()
+    cursor.exectue("SELECT * FROM persons")
+    myResault = cursor.fetchall()
 
     items = []
     for row in myResault:
-        items.append({"Firstname": row[0], "PersonID": row[1], "Lastname": row[2]})
-
-    connection().close()
+        items.append({"PersonID":row[0], "Firstname":row[1], "Lastname":row[2]})
+    cursor.close()
+    db.close()
     return json.dumps(items)
 
 @app.route('/person', method=['POST'])
 def post():
-    firstname = request.from('firstname')
-    lastname = request.from('lastname')
+    db = connection()
+    cursor = db.cursor()
+    reguests = request.from
+    firstname = reguests['firstname']
+    lastname = reguests['lastname']
     sql = "INSERT INTO persons (firstname, lastname) VALUES (%s,%s)", (firstname, lastname)
-    setCursor().execute(sql)
-    connection().commit()
-    connection().close()
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
     return str(firstname + lastname)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
