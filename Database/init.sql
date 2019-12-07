68fc288697e937ec1a2bbef4c91b9ca8981e9a08
Aleksander Henriksen <alhen18@student.sdu.dk>
2019-12-07 12:56:28 +0100

    commit

--- Database/init.sql
+++ Database/init.sql
@@ -2,7 +2,7 @@ create database person;
 use person;
 
 CREATE TABLE persons (
-  firstname CHAR(255) NOT NULL,
-  PersonID INT AUTO_INCREMENT PRIMARY KEY,
-  lastname CHAR(255) NOT NULL
+  PersonID INT(6) AUTO_INCREMENT PRIMARY KEY,
+  firstname TEXT,
+  lastname TEXT
 );
